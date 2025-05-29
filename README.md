5499ed66ed541b668a9623c755334bd737195a1f
Leandro Emanuel López <lopezlean@gmail.com>
2025-05-29 14:57:21 +0200

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+# powCAPTCHA Documentation
+
+Welcome to the powCAPTCHA documentation! This project is built upon the incredible Astro documentation framework, ensuring a seamless and well-documented experience for users and developers alike. If you're looking for detailed guides, explanations, and reference material for using Static Snap, you're in the right place.
+
+## Overview
+
+powCAPTCHA is a Next-gen Proof-of-Work Privacy-First CAPTCHA that stops bots and filter spam. Pure security, zero friction.
+
+
+## License
+
+powCAPTCHA is released under the MIT License. For more information, refer to the LICENSE file included in the project.
+
+## Credits
+
+This project is based on [Astro Docs](https://docs.astro.build/), and we acknowledge the amazing work done by the Astro developers. All copyright for the underlying documentation framework belongs to Astro's team.
---
+++ astro.config.ts
@@ -0,0 +1,94 @@
+import starlight from '@astrojs/starlight';
+import { pluginCollapsibleSections } from '@expressive-code/plugin-collapsible-sections';
+import { defineConfig, sharpImageService } from 'astro/config';
+import { makeLocalesConfig } from './config/locales';
+import { makeSidebar } from './config/sidebar';
+
+import rehypeSlug from 'rehype-slug';
+import remarkSmartypants from 'remark-smartypants';
+
+import { sitemap } from './integrations/sitemap';
+import { rehypeAutolink } from './plugins/rehype-autolink';
+import { rehypeTasklistEnhancer } from './plugins/rehype-tasklist-enhancer';
+import { remarkFallbackLang } from './plugins/remark-fallback-lang';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://docs.powcaptcha.com',
+	integrations: [
+		starlight({
+			title: 'Docs',
+			customCss: ['./src/styles/custom.css'],
+			expressiveCode: {
+				plugins: [pluginCollapsibleSections()],
+			},
+			components: {
+				EditLink: './src/components/starlight/EditLink.astro',
+				Head: './src/components/starlight/Head.astro',
+				Hero: './src/components/starlight/Hero.astro',
+				MarkdownContent: './src/components/starlight/MarkdownContent.astro',
+				MobileTableOfContents: './src/components/starlight/MobileTableOfContents.astro',
+				TableOfContents: './src/components/starlight/TableOfContents.astro',
+				PageSidebar: './src/components/starlight/PageSidebar.astro',
+				Pagination: './src/components/starlight/Pagination.astro',
+				Footer: './src/components/starlight/Footer.astro',
+				SiteTitle: './src/components/starlight/SiteTitle.astro',
+				//Search: './src/components/starlight/Search.astro',
+				Sidebar: './src/components/starlight/Sidebar.astro',
+				MobileMenuFooter: './src/components/starlight/MobileMenuFooter.astro',
+				PageTitle: './src/components/starlight/PageTitle.astro',
+			},
+			editLink: {
+				baseUrl: 'https://github.com/GetPowCaptcha/powcaptcha-docs/edit/main',
+			},
+			defaultLocale: 'en',
+			locales: makeLocalesConfig(),
+			sidebar: makeSidebar(),
+			social: {
+				github: 'https://github.com/GetPowCaptcha/powcaptcha-docs',
+				//discord: 'https://staticsnap.com/chat',
+			},
+			pagefind: true,
+			head: [
+				// Add ICO favicon fallback for Safari.
+				{
+					tag: 'link',
+					attrs: {
+						rel: 'icon',
+						href: '/favicon.ico',
+						sizes: '32x32',
+					},
+				},
+			],
+			disable404Route: true,
+		}),
+		sitemap(),
+	],
+	trailingSlash: 'always',
+	scopedStyleStrategy: 'where',
+	compressHTML: false,
+	markdown: {
+		// Override with our own config
+		smartypants: false,
+		remarkPlugins: [
+			[remarkSmartypants, { dashes: false }],
+			// Add our custom plugin that marks links to fallback language pages
+			remarkFallbackLang(),
+		],
+		rehypePlugins: [
+			rehypeSlug,
+			// This adds links to headings
+			...rehypeAutolink(),
+			// Tweak GFM task list syntax
+			rehypeTasklistEnhancer(),
+		],
+	},
+	image: {
+		domains: ['avatars.githubusercontent.com'],
+		service: sharpImageService(),
+	},
+	experimental: {
+		contentCollectionCache: false,
+		directRenderScript: true,
+	},
+});
